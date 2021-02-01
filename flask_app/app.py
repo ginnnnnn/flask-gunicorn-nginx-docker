a41e59e5277cda91583e181d5be5ddd76a71b7d6
ginnnnnn <aginlo@hotmail.com>
2021-02-01 14:04:54 +0800

    add todo app

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,16 +3,18 @@ from flask import Flask, request, jsonify
 
 server = Flask(__name__)
 
-
-def run_request():
-    index = int(request.json['index'])
-    list = ['red', 'green', 'blue', 'yellow', 'black']
-    return list[index]
+todo_list = []
 
 
-@server.route('/', methods=['GET', 'POST'])
+def add_todos():
+    todo = request.json['todo']
+    todo_list.append(todo)
+    return todo
+
+
+@server.route('/api/todos', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'GET':
-        return 'The model is up and running. Send a POST request'
+        return jsonify(todo_list)
     else:
-        return run_request()
+        return add_todos()
